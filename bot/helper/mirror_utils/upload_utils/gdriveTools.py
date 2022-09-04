c3c32ea7f2c0a43734be0d06dcd1ef825197699b
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-09-04 20:45:33 +0500

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -343,17 +343,17 @@ class GoogleDriveHelper:
                     self.deletefile(durl)
                     return "your clone has been stopped and cloned data has been deleted!", "cancelled"
                 if EMOJI_THEME is True:
-                    msg += f'<b>╭🗂️ Name: </b><code>{meta.get("name")}</code>'
-                    msg += f'\n<b>├📦 Size: </b>{get_readable_file_size(self.transferred_size)}'
-                    msg += f'\n<b>├💈 Type: </b>Folder'
-                    msg += f'\n<b>├🗃️ SubFolders: </b>{self.__total_folders}'
-                    msg += f'\n<b>├🗂️ Files: </b>{self.__total_files}'
+                    msg += f'<b>🗂️ Name: </b><code>{meta.get("name")}</code>'
+                    msg += f'\n<b>📦 Size: </b>{get_readable_file_size(self.transferred_size)}'
+                    msg += f'\n<b>💈 Type: </b>Folder'
+                    msg += f'\n<b>🗃️ SubFolders: </b>{self.__total_folders}'
+                    msg += f'\n<b>🗂️ Files: </b>{self.__total_files}'
                 else:
-                    msg += f'<b>╭ Name: </b><code>{meta.get("name")}</code>'
-                    msg += f'\n<b>├ Size: </b>{get_readable_file_size(self.transferred_size)}'
-                    msg += f'\n<b>├ Type: </b>Folder'
-                    msg += f'\n<b>├ SubFolders: </b>{self.__total_folders}'
-                    msg += f'\n<b>├ Files: </b>{self.__total_files}'
+                    msg += f'<b>Name: </b><code>{meta.get("name")}</code>'
+                    msg += f'\n<b>Size: </b>{get_readable_file_size(self.transferred_size)}'
+                    msg += f'\n<b>Type: </b>Folder'
+                    msg += f'\n<b>SubFolders: </b>{self.__total_folders}'
+                    msg += f'\n<b>Files: </b>{self.__total_files}'
                 buttons = ButtonMaker()
                 durl = short_url(durl)
                 buttons.buildbutton("☁️ Drive Link", durl)
@@ -365,9 +365,9 @@ class GoogleDriveHelper:
             else:
                 file = self.__copyFile(meta.get('id'), parent_id)
                 if EMOJI_THEME is True:
-                    msg += f'<b>╭🗂️ Name: </b><code>{file.get("name")}</code>'
+                    msg += f'<b>🗂️ Name: </b><code>{file.get("name")}</code>'
                 else:
-                    msg += f'<b>╭ Name: </b><code>{file.get("name")}</code>'
+                    msg += f'<b>Name: </b><code>{file.get("name")}</code>'
                 durl = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))
                 buttons = ButtonMaker()
                 durl = short_url(durl)
@@ -375,11 +375,11 @@ class GoogleDriveHelper:
                 if mime_type is None:
                     mime_type = 'File'
                 if EMOJI_THEME is True:
-                    msg += f'\n<b>├📦 Size: </b>{get_readable_file_size(int(meta.get("size", 0)))}'
-                    msg += f'\n<b>├💈 Type: </b>{mime_type}'
+                    msg += f'\n<b>📦 Size: </b>{get_readable_file_size(int(meta.get("size", 0)))}'
+                    msg += f'\n<b>💈 Type: </b>{mime_type}'
                 else:
-                    msg += f'\n<b>├ Size: </b>{get_readable_file_size(int(meta.get("size", 0)))}'
-                    msg += f'\n<b>├ Type: </b>{mime_type}'
+                    msg += f'\n<b>Size: </b>{get_readable_file_size(int(meta.get("size", 0)))}'
+                    msg += f'\n<b>Type: </b>{mime_type}'
                 if INDEX_URL is not None:
                     url_path = rquote(f'{file.get("name")}', safe='')
                     url = f'{INDEX_URL}/{url_path}'
@@ -786,34 +786,34 @@ class GoogleDriveHelper:
             if mime_type == self.__G_DRIVE_DIR_MIME_TYPE:
                 self.__gDrive_directory(meta)
                 if EMOJI_THEME is True:
-                    msg += f'<b>╭🗂️ Name: </b><code>{name}</code>'
-                    msg += f'\n<b>├📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├💈 Type: </b>Folder'
-                    msg += f'\n<b>├🗃️ SubFolders: </b>{self.__total_folders}'
+                    msg += f'<b>🗂️ Name: </b><code>{name}</code>'
+                    msg += f'\n<b>📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>💈 Type: </b>Folder'
+                    msg += f'\n<b>🗃️ SubFolders: </b>{self.__total_folders}'
                 else:
-                    msg += f'<b>╭ Name: </b><code>{name}</code>'
-                    msg += f'\n<b>├ Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├ Type: </b>Folder'
-                    msg += f'\n<b>├ SubFolders: </b>{self.__total_folders}'
+                    msg += f'<b>Name: </b><code>{name}</code>'
+                    msg += f'\n<b>Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>Type: </b>Folder'
+                    msg += f'\n<b>SubFolders: </b>{self.__total_folders}'
             else:
                 if EMOJI_THEME is True:
-                    msg += f'<b>╭🗂️ Name: </b><code>{name}</code>'
+                    msg += f'<b>🗂️ Name: </b><code>{name}</code>'
                 else:
-                    msg += f'<b>╭ Name: </b><code>{name}</code>'
+                    msg += f'<b>Name: </b><code>{name}</code>'
                 if mime_type is None:
                     mime_type = 'File'
                 self.__total_files += 1
                 self.__gDrive_file(meta)
                 if EMOJI_THEME is True:
-                    msg += f'\n<b>├📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├💈 Type: </b>{mime_type}'
+                    msg += f'\n<b>📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>💈 Type: </b>{mime_type}'
                 else:
-                    msg += f'\n<b>├ Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├ Type: </b>{mime_type}'
+                    msg += f'\n<b>Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>Type: </b>{mime_type}'
             if EMOJI_THEME is True:
-                msg += f'\n<b>├🗂️ Files: </b>{self.__total_files}'
+                msg += f'\n<b>🗂️ Files: </b>{self.__total_files}'
             else:
-                msg += f'\n<b>├ Files: </b>{self.__total_files}'
+                msg += f'\n<b>Files: </b>{self.__total_files}'
         except Exception as err:
             if isinstance(err, RetryError):
                 LOGGER.info(f"Total Attempts: {err.last_attempt.attempt_number}")
