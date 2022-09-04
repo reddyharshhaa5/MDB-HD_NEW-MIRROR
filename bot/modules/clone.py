efc7814830e5aefcc357b89d387bb06d8b673221
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-09-04 13:22:36 +0500

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -216,9 +216,9 @@ def _clone(message, bot):
             except IndexError:
                 pass
         if EMOJI_THEME is True:
-            cc = f'\n<b>╰👤 cc: </b>{tag}\n\n'
+            cc = f'\n<b>╰👤 User: </b>{tag}\n\n'
         else:
-            cc = f'\n<b>╰ cc: </b>{tag}\n\n'
+            cc = f'\n<b>╰ User: </b>{tag}\n\n'
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", bot, message)
         else:
