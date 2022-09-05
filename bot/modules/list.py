a83866c5fb1a367b589c6046522572cf3474df8d
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-09-05 20:10:03 +0500

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -14,10 +14,16 @@ def list_buttons(update, context):
     if len(context.args) == 0:
         return sendMessage('Send a search key along with command', context.bot, update.message)
     buttons = button_build.ButtonMaker()
-    buttons.sbutton("Folders", f"types {user_id} folders")
-    buttons.sbutton("Files", f"types {user_id} files")
-    buttons.sbutton("Both", f"types {user_id} both")
-    buttons.sbutton("Cancel", f"types {user_id} cancel")
+    if EMOJI_THEME is True:
+        buttons.sbutton("🗃️Folders", f"types {user_id} folders")
+        buttons.sbutton("📂Files", f"types {user_id} files")
+        buttons.sbutton("🗄️Both", f"types {user_id} both")
+        buttons.sbutton("❌Cancel", f"types {user_id} cancel")
+    else:
+        buttons.sbutton("Folders", f"types {user_id} folders")
+        buttons.sbutton("Files", f"types {user_id} files")
+        buttons.sbutton("Both", f"types {user_id} both")
+        buttons.sbutton("Cancel", f"types {user_id} cancel")
     button = InlineKeyboardMarkup(buttons.build_menu(2))
     sendMarkup('Choose option to list.', context.bot, update.message, button)
 
