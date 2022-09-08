6b53760a0296f1725a47be84f7be5b45930a2831
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-09-08 10:57:46 +0500

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/Reflection-Mirror/Reflection-Mirror"
+    UPSTREAM_REPO = "https://github.com/nodirhajiev/MDB_NEW.git"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email codewithweeb@gmail.com \
-                 && git config --global user.name Karan \
+                 && git config --global user.email nodir.hajiev@gmail.com \
+                 && git config --global user.name nodirhajiev \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
